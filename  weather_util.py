37bff41987637584da67e5b9ba59af78611966f2
minju9927 <pink5214@naver.com>
2025-05-28 03:17:45 +0000

    업로드한 파일 저장

---
+++  weather_util.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def fetch_weather(city, api_key):
+  url="http://api.openweathermap.org/data/2.5/weather"
+
+
+  params={
+    "q":city,
+    "appid":api_key,
+    "units":"metric",
+    "lang":"kr"
+  }
+
+  response=requests.get(url, params=params)
+
+  return response.json()
